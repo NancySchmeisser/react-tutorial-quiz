2037689e98460c5ea4fe5a1f8e2d468ff7c47df9
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-02-21 08:40:05 +0100

    feat:  added quizComplete

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -9,6 +9,7 @@ interface QuizProps {
 interface QuizState {
   exerciseList: Array<{ question: string, answer: string, correctAnswer: string }>,
   currentExercise: number,
+  quizComplete: boolean,
 }
 class Quiz extends Component<QuizProps, QuizState> {
   constructor(props: QuizProps) {
@@ -22,23 +23,30 @@ class Quiz extends Component<QuizProps, QuizState> {
         { question: "2 + 4", answer: "", correctAnswer: "6" },
       ],
       currentExercise: 0,
+      quizComplete: false,
 
     }
   }
-  handleOnAnswered= (answer: string) => {
+  handleOnAnswered = (answer: string) => {
 
-    const modifiedExerciseList= this.state.exerciseList.slice(0);
+    const modifiedExerciseList = this.state.exerciseList.slice(0);
     modifiedExerciseList[this.state.currentExercise].answer = answer;
-
+    if (this.state.currentExercise === this.state.exerciseList.length-1) {
+      this.setState({quizComplete: true})
+    } 
     this.setState({
       exerciseList: modifiedExerciseList,
-      currentExercise: this.state.currentExercise +1,
+      currentExercise: this.state.currentExercise + 1,
     })
   }
   render() {
-    return (
-      <Exercise question={this.state.exerciseList[this.state.currentExercise].question} onAnswered={this.handleOnAnswered} />
-    );
+    if (this.state.quizComplete === true) {
+      return <div>Fertig!</div>
+    } else {
+      return (
+        <Exercise question={this.state.exerciseList[this.state.currentExercise].question} onAnswered={this.handleOnAnswered} />
+      );
+    }
   }
 }
 
